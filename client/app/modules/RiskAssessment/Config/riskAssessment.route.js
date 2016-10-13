6d33aecec43846f3eb3553f188eb73003ac0e6b5
John <jlister76@gmail.com>
2016-10-13 17:00:55 -0500

    2nd commit

--- client/app/modules/RiskAssessment/Config/riskAssessment.route.js
+++ client/app/modules/RiskAssessment/Config/riskAssessment.route.js
@@ -7,16 +7,21 @@
       .state('portal.riskAssessment', {
         abstract: true,
         url: '/risk-assessment',
-        template: '<div layout="column" layout-align="start center" style="width:auto;"><p class="md-title">Risk Assessment</p><div ui-view layout layout-align="center start" style="width:100%;"></div></div>'
+        templateUrl: 'modules/RiskAssessment/Views/base.html'
       })
-        .state('portal.riskAssessment.home', {
+        .state('portal.riskAssessment.menu', {
           url: '',
-          templateUrl: 'modules/RiskAssessment/Views/base.html',
+          templateUrl: 'modules/RiskAssessment/Views/menu.html',
           controller: 'RiskAssessmentCtrl'
         })
         .state('portal.riskAssessment.new', {
           url: '/new',
           templateUrl: 'modules/RiskAssessment/Views/new-assessment.html',
+          controller: 'RiskAssessmentCtrl'
+        })
+        .state('portal.riskAssessment.step1', {
+          url: '/step1',
+          templateUrl: 'modules/RiskAssessment/Views/step1.html',
           controller: 'RiskAssessmentCtrl'
         })
     })
